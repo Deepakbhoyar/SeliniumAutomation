44ff50b5d2c3b27aff30517c584def75a0e04cde
deepak <deepakbhoyar2217@gmail.com>
2025-08-14 13:43:16 +0530

    ISTFP-1234 Making the changes by adding new class

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    WebDriver driver;
+
+    @FindBy(id = "username")
+    WebElement username ;
+
+    @FindBy(id = "password")
+    WebElement password ;
+
+    @FindBy(id = "login")
+    WebElement login ;
+
+    public LoginPage (WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    public void page (String user, String pass){
+        username.sendKeys(user);
+        password.sendKeys(pass);
+        login.click();
+    }
+
+
+}
--- src/test/java/testCases/WikipediaSearchTest.java
+++ src/test/java/testCases/WikipediaSearchTest.java
@@ -1,7 +1,6 @@
 package testCases;
 
 import baseTestCase.BaseTest;
-import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
